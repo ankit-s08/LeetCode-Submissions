20058e4525b7106137c0cdb18175ca2e1739081c
Ankit Sahu <95268596+ankit-s08@users.noreply.github.com>
2022-03-18 23:55:52 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.48%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     int fib(int n) {
+        int a=0;
+        int b=1;
+        int sum=a+b;
+        int i=0;
+        
         if(n==0)
             return 0;
         else if(n==1)
             return 1;
-        return fib(n-1)+fib(n-2);
+        while(n>2)
+        {
+            
+            //cout<<sum<<" ";
+            a=b;
+            b=sum;
+            
+            sum = a+b;
+            n--;
+        }
+        return sum;
     }
 };
